3e46b2a7916c02f38620df39798fdf936426cee3
vinensiuswibowo <hello@vinensiuswibowo.com>
2020-07-24 18:46:24 +0700

    fix svg icon

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -2,30 +2,30 @@ import React, { ReactNode } from 'react';
 import styles from './Checkbox.module.css';
 
 const CheckedIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24">
+  <svg width={24} height={24} viewBox="0 0 24 24">
     <defs>
       <linearGradient id="a" x1="13.368%" x2="87.636%" y1="16.036%" y2="81.312%">
-        <stop offset="0%" stop-color="#6BAA12"></stop>
-        <stop offset="100%" stop-color="#009E9B"></stop>
+        <stop offset="0%" stopColor="#6BAA12" />
+        <stop offset="100%" stopColor="#009E9B" />
       </linearGradient>
     </defs>
-    <g fill="none" fill-rule="evenodd">
+    <g fill="none" fillRule="evenodd">
       <path
         fill="url(#a)"
         d="M11 0C4.923 0 0 4.923 0 11s4.923 11 11 11 11-4.923 11-11S17.077 0 11 0z"
         transform="translate(1 1)"
-      ></path>
+      />
       <path
         fill="#FFF"
         d="M16.364 7.61l-6.284 6.22-2.044-2.02a.38.38 0 0 0-.535 0l-.89.883a.373.373 0 0 0 0 .53l3.202 3.17a.38.38 0 0 0 .534 0l7.442-7.369a.373.373 0 0 0 0-.53l-.89-.884a.38.38 0 0 0-.535 0z"
-      ></path>
+      />
     </g>
   </svg>
 );
 
 const DisabledCheckedIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-    <g fill="none" fill-rule="evenodd">
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+    <g fill="none" fillRule="evenodd">
       <path
         fill="#C7C7C7"
         d="M11 0C4.923 0 0 4.923 0 11s4.923 11 11 11 11-4.923 11-11S17.077 0 11 0z"
@@ -41,16 +41,16 @@ const DisabledCheckedIcon = () => (
 );
 
 const UnCheckedIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24">
-    <g fill="none" fill-rule="evenodd">
+  <svg width={24} height={24} viewBox="0 0 24 24">
+    <g fill="none" fillRule="evenodd">
       <path
         fill="#C7C7C7"
         d="M12 1c6.077 0 11 4.923 11 11s-4.923 11-11 11S1 18.077 1 12 5.923 1 12 1zm0 1C6.476 2 2 6.476 2 12s4.476 10 10 10 10-4.476 10-10S17.524 2 12 2z"
-      ></path>
+      />
       <path
         fill="#ECECEC"
         d="M12 2C6.476 2 2 6.476 2 12s4.476 10 10 10 10-4.476 10-10S17.524 2 12 2z"
-      ></path>
+      />
     </g>
   </svg>
 );
--- src/components/Select/ModalSelect.tsx
+++ src/components/Select/ModalSelect.tsx
@@ -4,11 +4,11 @@ import Button from '../Button';
 import styles from './Select.module.css';
 
 const SearchSvg = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" style={{ marginLeft: 15 }}>
+  <svg width={24} height={24} viewBox="0 0 24 24">
     <path
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M19.86 18.625l-3.787-3.787c-.071-.072-.165-.109-.265-.109h-.412c.983-1.138 1.579-2.62 1.579-4.242C16.975 6.904 14.07 4 10.487 4 6.904 4 4 6.904 4 10.487c0 3.584 2.904 6.488 6.487 6.488 1.622 0 3.104-.596 4.242-1.579v.412c0 .1.04.194.11.265l3.786 3.787c.146.146.383.146.53 0l.705-.705c.146-.147.146-.384 0-.53zm-9.373-3.147c-2.757 0-4.99-2.234-4.99-4.99 0-2.758 2.233-4.99 4.99-4.99s4.99 2.232 4.99 4.99c0 2.756-2.233 4.99-4.99 4.99z"
-    ></path>
+    />
   </svg>
 );
 
@@ -49,7 +49,15 @@ export default function ModalSelect(props: {
   return (
     <div style={{ padding: '15px 20px 0px 20px' }}>
       <label style={{ fontWeight: 700 }}>{props.label}</label>
-      <Input icon={<SearchSvg />} placeholder="Tap to search" onChange={onChange} />
+      <Input
+        icon={
+          <span style={{ marginLeft: 15 }}>
+            <SearchSvg />
+          </span>
+        }
+        placeholder="Tap to search"
+        onChange={onChange}
+      />
       <div style={{ overflow: 'auto', height: 'calc(100vh - 130px)' }}>
         {list
           .filter((d: any) =>
