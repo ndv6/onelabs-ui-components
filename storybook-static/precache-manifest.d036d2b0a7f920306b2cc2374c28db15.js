aa0db47fc84a4a24fb363f7c113bb11bc90332df
vinensiuswibowo <hello@vinensiuswibowo.com>
2020-07-28 17:32:59 +0700

    add storybook

---
+++ storybook-static/precache-manifest.d036d2b0a7f920306b2cc2374c28db15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5d18ed761070e010e1db3554a27ed2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e0ce862c045f9330a2cf.bundle.js"
+  },
+  {
+    "url": "runtime~main.e0ce862c045f9330a2cf.bundle.js"
+  },
+  {
+    "revision": "36fb0cf6c12b5d25ef88",
+    "url": "static/css/main.f0d5d22c.chunk.css"
+  },
+  {
+    "url": "vendors~main.e0ce862c045f9330a2cf.bundle.js"
+  },
+  {
+    "url": "vendors~main.e0ce862c045f9330a2cf.bundle.js.LICENSE.txt"
+  }
+]);
--- storybook-static/service-worker.js
+++ storybook-static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.6339592d8a519e3427f44420dd08bb2d.js"
+  "precache-manifest.d036d2b0a7f920306b2cc2374c28db15.js"
 );
 
 self.addEventListener('message', (event) => {
