9e6565a5462dc374d9a0e1167d0eb22e2fb7b476
sekar.minati <sekar.minati@ocbcnisp.com>
2021-09-20 14:22:41 +0700

    modify modal

--- dist/Modal/index.js
+++ dist/Modal/index.js
@@ -4,14 +4,8 @@ import Button from '../Button';
 import Card from '../Card';
 import { createClassName } from '../helpers';
 import styles from './Modal.module.css';
-var ArrowSvg = function () { return (React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24" },
-    React.createElement("g", { fill: "none", fillRule: "evenodd" },
-        React.createElement("g", null,
-            React.createElement("g", null,
-                React.createElement("g", { fill: "#939FAB", transform: "translate(-335 -110) translate(335 110)" },
-                    React.createElement("circle", { cx: "12", cy: "12", r: "12" })),
-                React.createElement("g", { fill: "#FFF" },
-                    React.createElement("path", { d: "M11.463 1.35c.252-.186.61-.166.838.063.252.251.252.659 0 .91L7.766 6.857l4.535 4.536c.252.251.252.659 0 .91-.228.228-.586.25-.838.062l-.072-.062-4.535-4.536-4.533 4.536-.072.062c-.252.187-.61.166-.838-.062-.251-.251-.251-.659 0-.91l4.533-4.536-4.533-4.534c-.251-.251-.251-.659 0-.91.228-.229.586-.25.838-.062l.072.062 4.533 4.534 4.535-4.534z", transform: "translate(-335 -110) translate(335 110) translate(5.143 5.143)" }))))))); };
+var ArrowSvg = function () { return (React.createElement("svg", { width: "10", height: "10", viewBox: "0 0 10 10", fill: "none", xmlns: "http://www.w3.org/2000/svg" },
+    React.createElement("path", { "fill-rule": "evenodd", "clip-rule": "evenodd", d: "M6.46706 5.01575L9.82282 1.65999C10.0169 1.46591 10.0169 1.15095 9.82282 0.956562L9.04307 0.176811C8.84899 -0.0172703 8.53403 -0.0172703 8.33964 0.176811L4.98419 3.53289L1.62843 0.177122C1.43435 -0.0169587 1.11939 -0.0169587 0.925 0.177122L0.145561 0.956562C-0.0485203 1.15064 -0.0485203 1.4656 0.145561 1.65999L3.50132 5.01575L0.145561 8.37152C-0.0485203 8.5656 -0.0485203 8.88055 0.145561 9.07494L0.925312 9.85469C1.11939 10.0488 1.43435 10.0488 1.62874 9.85469L4.98419 6.49862L8.33995 9.85438C8.53403 10.0485 8.84899 10.0485 9.04338 9.85438L9.82313 9.07463C10.0172 8.88055 10.0172 8.5656 9.82313 8.3712L6.46706 5.01575Z", fill: "#313131" }))); };
 var classNames = createClassName(styles);
 var body = document.getElementsByTagName('body')[0];
 if (!document.getElementById('modal-portal')) {
@@ -42,7 +36,8 @@ function Modal(props) {
         return ReactDOM.createPortal(React.createElement("div", { className: classnames },
             React.createElement(Card, null,
                 React.createElement("div", { className: styles.content },
-                    !props.hideCloseButton && (React.createElement(Button, { className: styles.close, onClick: props.onClose }, "X")),
+                    !props.hideCloseButton && (React.createElement(Button, { className: styles.close, onClick: props.onClose },
+                        React.createElement(ArrowSvg, null))),
                     React.createElement("div", null, props.children)))), el);
     }
     return ReactDOM.createPortal(React.createElement("div", { className: classnames },
