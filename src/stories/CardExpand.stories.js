90feba794d3f1d8a2e556b9f1dcedcf814b03016
sayaandreas <andreas.ocbcnisp@gmail.com>
2021-11-16 16:42:30 +0700

    fix card animation

--- src/stories/CardExpand.stories.js
+++ src/stories/CardExpand.stories.js
@@ -7,7 +7,7 @@ export const Default = () => {
   const [state, setstate] = React.useState(false);
   return (
     <div>
-      <button onClick={() => setstate(true)}>show card</button>
+      <button onClick={() => setstate(!state)}>toggle card</button>
       <CardExpand active={state}>
         {{
           title: <Text.Title>Lorem Ipsum</Text.Title>,
@@ -36,7 +36,7 @@ export const WithoutArrow = () => {
   const [state, setstate] = React.useState(false);
   return (
     <div>
-      <button onClick={() => setstate(true)}>show card</button>
+      <button onClick={() => setstate(!state)}>toggle card</button>
       <CardExpand active={state} closeIcon={false}>
         {{
           title: (
@@ -65,8 +65,37 @@ export const WithoutExpand = () => {
   const [state, setstate] = React.useState(false);
   return (
     <div>
-      <button onClick={() => setstate(true)}>show card</button>
+      <button onClick={() => setstate(!state)}>toggle card</button>
       <CardExpand active={state} expand={false} onClose={() => setstate(false)}>
+        {{
+          title: <Text.Title>Lorem Ipsum</Text.Title>,
+          content: (
+            <>
+              Excepteur laborum eu dolore commodo. Ea minim nostrud excepteur duis fugiat cillum
+              exercitation ex occaecat nisi. Non ea duis deserunt adipisicing sit labore laboris id
+              ex sunt sint aliquip incididunt. Aliqua sint irure in proident laborum cillum magna
+              cupidatat. Velit labore officia consequat sint consequat sit sunt ullamco
+              reprehenderit aliqua reprehenderit tempor veniam sint. Veniam velit dolore in qui ea
+              et commodo ut elit nisi. Ex tempor cillum et eu sit ullamco.
+            </>
+          ),
+          action: (
+            <Button variant="primary" full>
+              Button
+            </Button>
+          ),
+        }}
+      </CardExpand>
+    </div>
+  );
+};
+
+export const Success = () => {
+  const [state, setstate] = React.useState(false);
+  return (
+    <div>
+      <button onClick={() => setstate(!state)}>toggle card</button>
+      <CardExpand active={state} variant="success">
         {{
           title: <Text.Title>Lorem Ipsum</Text.Title>,
           content: (
